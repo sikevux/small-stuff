7ea583b60f5129255dc3f28f4806f32aac19e4ec
Fredrik Strandin <fredrik@empedokles.(none)>
2011-05-17 16:20:03 +0200

    This code needed some tighten up. Fixed intendenting and made it valid xhtml 1.0 strict.

--- disk.php
+++ disk.php
@@ -6,124 +6,124 @@
  *
  * Contributions by: kd35a
  */
-?>
-<?php
+
 function percent($amount, $total) {
-$count = $amount / $total;
-$countt = $count * 100;
-$result = number_format($countt, 0);
-//echo $result;
+	$count = $amount / $total;
+	$countt = $count * 100;
+	$result = number_format($countt, 0);
+	//echo $result;
 }
+
 $disc_count = shell_exec("df -Pk|grep -v none|wc -l");
 $disc_name = shell_exec("df -Pk|grep -v none|awk -v col=1 'NR > 1 {sub( \"\", \"\", $col); print $col }'");
+
 for($i=1; $i<$disc_count; $i++) {
-$drive_array[] = shell_exec("df -Pk|grep -v none|awk col=1 'NR > 1 {sub( \"\", \"\", \$col); print \$col }'|sed -n '".$i."p'");
-$drive_max[] =  shell_exec("df -Pk|grep -v none|awk col=4 'NR > 1 {sub( \"\", \"\", \$col); print \$col }'|sed -n '".$i."p'");
-$drive_use_b[] =  shell_exec("df -Pk|grep -v none|awk col=3 'NR > 1 {sub( \"\", \"\", \$col); print \$col }'|sed -n '".$i."p'");
-$drive_use[] =  shell_exec("df -Pk|grep -v none|awk col=5 'NR > 1 {sub( \"\", \"\", \$col); print \$col }'|sed -n '".$i."p'");
+	$drive_array[] = shell_exec("df -Pk|grep -v none|awk col=1 'NR > 1 {sub( \"\", \"\", \$col); print \$col }'|sed -n '".$i."p'");
+	$drive_max[] =  shell_exec("df -Pk|grep -v none|awk col=4 'NR > 1 {sub( \"\", \"\", \$col); print \$col }'|sed -n '".$i."p'");
+	$drive_use_b[] =  shell_exec("df -Pk|grep -v none|awk col=3 'NR > 1 {sub( \"\", \"\", \$col); print \$col }'|sed -n '".$i."p'");
+	$drive_use[] =  shell_exec("df -Pk|grep -v none|awk col=5 'NR > 1 {sub( \"\", \"\", \$col); print \$col }'|sed -n '".$i."p'");
 }
+
 $system_max = shell_exec("df -Pk --total|awk col=4 'NR > 1 {sub( \"\", \"\", \$col); print \$col }'| tail -n1");
+
 for($i=1; $i<$disc_count; $i++) {
-$drive_percent[] = percent($drive_max[$i], $system_max);
+	$drive_percent[] = percent($drive_max[$i], $system_max);
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title><?php system(hostname); ?> status</title>
-<meta charset="utf-8">
-<meta content="width=400" name="viewport">
-<style type="text/css">
-body {
-	margin: 14% 5% 0;
-}
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+	<head>
+		<title><?php system(hostname); ?> status</title>
+		<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />
+		<meta content="width=400" name="viewport" />
+		<style type="text/css">
+		body {
+			margin: 14% 5% 0;
+		}
 
-h1,h2 {
-	font: 1em sans-serif;
-	margin: 2pt 0;
-}
+		h1,h2 {
+			font: 1em sans-serif;
+			margin: 2pt 0;
+		}
 
-h1 {
-	font-size: 3em;
-	text-align: center;
-}
+		h1 {
+			font-size: 3em;
+			text-align: center;
+		}
 
-h1+h2 {
-	text-align: center;
-}
+		h1+h2 {
+			text-align: center;
+		}
 
-h2 {
-	font-size: 1.2em;
-	margin-left: 1em;
-}
+		h2 {
+			font-size: 1.2em;
+			margin-left: 1em;
+		}
 
-h3 {
-	margin-top: 15em;
-	font-size: 12px;
-	text-align: center;
-}
+		h3 {
+			margin-top: 15em;
+			font-size: 12px;
+			text-align: center;
+		}
 
-.drive,.partition,.used-space {
-	float: left;
-}
+		.drive,.partition,.used-space {
+			float: left;
+		}
 
-.drive {
-	width: 100%;
-	outline: 1px solid black;
-	margin: 1ex0;
-}
+		.drive {
+			width: 100%;
+			outline: 1px solid black;
+			margin: 1ex0;
+		}
 
-.partition {
-	background-color: #80ff80;
-}
+		.partition {
+			background-color: #80ff80;
+		}
 
-.used-space {
-	white-space: nowrap;
-	background-color: #ff8080;
-	overflow: visible;
-}
-<?php
-for($i=1; $i<$disc_count; $i++) {
-print("#".$drive_array[$i].".partition { width: ".$drive_percent[$i]."%;}");
-print("#".$drive_array[$i].".partition .used-space { width: ".$drive_use[$i].";}");
-}
-?>
-
-</style>
-</head>
-<body>
-	<h1>
-	<?php system("hostname"); echo $drive_array[1];?>
-	</h1>
-	<?php
-	$updata = shell_exec('uptime');
-	preg_match("/(up[ ]{1,}(([\d]{1,3}[ ]{1,}((day)|(days)),[ ]{1,})?([\d]{1,2}:)?[\d]{1,2}( min)?))(,[ \da-z]{0,},[ a-z:]{1,})([\d]{1,}.[\d]{2}, [\d]{1,}.[\d]{2}, [\d]{1,}.[\d]{2})/", $updata, $tmp);
-	$uptime = $tmp[2];
-	$load = explode('average:', $updata);
-	$kdata = shell_exec('uname -sr');
-	$kernel = explode('-', $kdata);
-	?>
-	<h2>
-	<?php echo $kernel[0]; ?>
-		&ndash; up
-		<?php echo $uptime; //echo $boot[1]; ?>
-	</h2>
-	<h2>disk sda</h2>
-	<div class="drive" id="sda">
-		<div class="partition" id="sda1">
-			<div class="used-space">&nbsp;sda1 on /boot</div>
+		.used-space {
+			white-space: nowrap;
+			background-color: #ff8080;
+			overflow: visible;
+		}
+		<?php
+		for($i=1; $i<$disc_count; $i++) {
+		print("#".$drive_array[$i].".partition { width: ".$drive_percent[$i]."%;}");
+		print("#".$drive_array[$i].".partition .used-space { width: ".$drive_use[$i].";}");
+		}
+		?>
+		</style>
+	</head>
+	<body>
+		<h1>
+			<?php system("hostname"); echo $drive_array[1];?>
+		</h1>
+		<?php
+		$updata = shell_exec('uptime');
+		preg_match("/(up[ ]{1,}(([\d]{1,3}[ ]{1,}((day)|(days)),[ ]{1,})?([\d]{1,2}:)?[\d]{1,2}( min)?))(,[ \da-z]{0,},[ a-z:]{1,})([\d]{1,}.[\d]{2}, [\d]{1,}.[\d]{2}, [\d]{1,}.[\d]{2})/", $updata, $tmp);
+		$uptime = $tmp[2];
+		$load = explode('average:', $updata);
+		$kdata = shell_exec('uname -sr');
+		$kernel = explode('-', $kdata);
+		?>
+		<h2>
+			<?php echo $kernel[0]." &ndash; up ".$uptime; ?>
+		</h2>
+		<h2>disk sda</h2>
+		<div class="drive" id="sda">
+			<div class="partition" id="sda1">
+				<div class="used-space">&nbsp;sda1 on /boot</div>
+			</div>
+			<div class="partition" id="sda5">
+				<div class="used-space">&nbsp;sda5 on /</div>
+			</div>
 		</div>
-		<div class="partition" id="sda5">
-			<div class="used-space">&nbsp;sda5 on /</div>
+		<p />
+		<h2>disk sdb</h2>
+		<div class="drive" id="sdb">
+			<div class="partition" id="sdb1">
+				<div class="used-space">&nbsp;sdb1 on /pool</div>
+			</div>
 		</div>
-	</div>
-	<br />
-	<h2>disk sdb</h2>
-	<div class="drive" id="sdb">
-		<div class="partition" id="sdb1">
-			<div class="used-space">&nbsp;sdb1 on /pool</div>
-		</div>
-	</div>
-	<h3>:3</h3>
-</body>
+		<h3>:3</h3>
+	</body>
 </html>
