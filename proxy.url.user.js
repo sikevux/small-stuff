5aa2528eb2fb462424732f75fe4cd8544ae96667
Sikevux <sikevux@sikevux.se>
2011-06-08 23:49:50 +0200

    Greasemonky scriptfor prx

---
+++ proxy.url.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name          Prx Auto redirect
+// @namespace	*
+// @description   Auto redirect to prx
+// @include       *
+// ==/UserScript==
+
+var regExp = /facebook/i;
+if (regExp.test(window.location.href))
+    window.location.replace(window.location.href.replace(/facebook/, "fxcxbxxk"));
