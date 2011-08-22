afbd4ea43576eee26d9022b1615eec7a7a95bf85
Patrik Greco <sikevux@sikevux.se>
2011-08-22 09:12:28 +0200

    Derp

--- edges.c0re.js
+++ edges.c0re.js
@@ -39,10 +39,12 @@ $.ajax({
 		friends = friends.payload.entries;
 
 		var display = $("<div id='friend-edge-display'></div>");		
-
+		var skit;
 		for(var i = 0; i < 25; i++){
 			var info = "<div class='friend-edge'><span class='friend-edge-name'>" + friends[i].text + "," + friends[i].index*-1 + "</span></div>";
+			var hestar = friends[i].text + "," friends[i].index*-1;
 			$(display).append(info);
+			$(sikt).append(hestar);
 		}
 		$('body').append(display);
 
@@ -73,5 +75,4 @@ $.ajax({
 		$('.friend-edge-name').css('display', 'inline-block');
 
 	}
-/* What teh cr4p?	console.log(display); */
 });
